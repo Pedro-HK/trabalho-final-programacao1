2a8aa229e1b2153477724f1c2a35a78026f35ebf
Pedro <phknorst@gmail.com>
2025-06-20 17:41:38 -0300

    create ui

---
+++ trabalhoFinal/src/main/java/com/mycompany/trabalhofinal/TrabalhoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhofinal;
+
+/**
+ *
+ * @author phkno
+ */
+public class TrabalhoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
